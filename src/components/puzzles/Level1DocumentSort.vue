9d43c354f34d494709a767dae47d625b129f93ec
Ryan Poppe <ryan.poppe@vasion.com>
2025-12-18 14:22:20 -0700

    build

--- src/components/puzzles/Level1DocumentSort.vue
+++ src/components/puzzles/Level1DocumentSort.vue
@@ -109,7 +109,9 @@ function shuffle<T>(array: T[]): T[] {
   const shuffled = [...array]
   for (let i = shuffled.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
-    ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+    const temp = shuffled[i]
+    shuffled[i] = shuffled[j] as T
+    shuffled[j] = temp as T
   }
   return shuffled
 }
